556ecc36129d456eb7537bea52ac7f11b64fbccc
DESKTOP-92KNQDD\lider <lider.x2011@yandex.ru>
2022-03-10 13:01:34 +0300

    Anim - Run; Walk; Jump;

--- 2D_RPG/Assets/Scripts/Sc_Charecter.cs
+++ 2D_RPG/Assets/Scripts/Sc_Charecter.cs
@@ -8,11 +8,18 @@ public class Sc_Charecter : MonoBehaviour
     [SerializeField] private float speed = 3.0F;
     [SerializeField] private float HP = 10;
     [SerializeField] private float jumpForce = 10.0F;
+    private bool isGround = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
 
     private Rigidbody2D rigibody;
     private Animator animator;
     private SpriteRenderer sprite;
-    private bool isGround = false;
+
     private void Awake()
     {
         rigibody = GetComponent<Rigidbody2D>();
@@ -25,6 +32,8 @@ public class Sc_Charecter : MonoBehaviour
     }
     private void Update()
     {
+        if (isGround) State = CharState.Idle;
+
         if (Input.GetButton("Horizontal")) Run();
         if (isGround && Input.GetButtonDown("Jump")) Jump();
     }
@@ -35,15 +44,24 @@ public class Sc_Charecter : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, transform.position + derection, speed * Time.deltaTime);
 
         sprite.flipX = derection.x < 0.0F;
+
+        if (isGround) State = CharState.Walk;
     }
 
     private void Jump()
     {
         rigibody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        State = CharState.Jump;
     }
     private void CheckGround()
     {
         Collider2D[] colider = Physics2D.OverlapCircleAll(transform.position, 1F);
         isGround = colider.Length > 1;
     }
+}
+public enum CharState
+{
+    Idle,
+    Walk,
+    Jump
 }
